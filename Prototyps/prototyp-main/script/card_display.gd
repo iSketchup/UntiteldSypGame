22478415149ace10c6142fc3c948bb261b210623
iSketchup <kalulu@gmx.at>
2026-04-13 20:59:22 +0200

    wip
    drag and drop ist arschloch

--- Prototyps/prototyp-main/script/card_display.gd
+++ Prototyps/prototyp-main/script/card_display.gd
@@ -1,20 +1,14 @@
 extends CanvasLayer
-var Card_Bones = preload("res://scene/Card_Bones.tscn")
+var Card_Skin = preload("res://scene/Card_Skin.tscn")
 
+var Hand =  []
 
 @onready var Display = $Display/Con_Display
 
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func add_HandCard(link: String)-> void:
 	var card_resource = load(link)
-	var new = Card_Bones.instantiate()
+	var new = Card_Skin.instantiate()
 	
 	var cover:TextureRect = new.find_child('Cover')
 	cover.texture = card_resource.image
@@ -22,9 +16,34 @@ func add_HandCard(link: String)-> void:
 	Description.text = card_resource.Description
 	
 	Display.add_child(new)
+	Hand.append(new)
+	
 
 func _get_drag_data(_pos):
+
 	var preview = duplicate()
 	preview.modulate.a = 0.6
-	set_drag_preview(preview)
+	
+	var cr := ColorRect.new()
+	cr.color = Color.ORANGE_RED
+	cr.custom_minimum_size = Vector2(200, 100)
+	
+	preview.add_child(cr)
+	## set_drag_preview(preview)
+	
+	print('Ive been touched')
+	
 	return self
+
+
+
+func _can_drop_data(_pos, data):
+	return typeof(data) == typeof(Card_Skin)
+	
+
+func _drop_data(_pos, data):
+	var dad = data.get_parent()
+	
+	dad.remove_child(data)
+	
+	Display.add_child(data)
