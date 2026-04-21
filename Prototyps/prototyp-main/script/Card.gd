5e221500f3fa528a3a8b46c1f824c63bfeaddd86
iSketchup <kalulu@gmx.at>
2026-04-21 10:31:07 +0200

    Card script

--- Prototyps/prototyp-main/script/Card.gd
+++ Prototyps/prototyp-main/script/Card.gd
@@ -1,3 +1,4 @@
+@tool
 extends Resource
 
 class_name Card
@@ -6,31 +7,85 @@ class_name Card
 
 @export var Description :String
 
-
+@export_group('Triggers')
+@export var onTrigger:bool = false 
+@export var onBought:bool = false 
+@export var onDrawn:bool = false 
+@export var onPlaced:bool = false 
+@export var onGotPlacedOn:bool = false 
+@export var onDiscarded:bool = false 
 
 
 
 
 ## Card Funcs
 func placed():
-	pass
+	if !onPlaced:
+		return
+	callFunc()
 
 func trigger():
-	print('triggered')
+	if !onTrigger:
+		return
 	callFunc()
 
 func discarded():
-	pass
+	if !onDiscarded:
+		return
+	callFunc()
 
 func gotplacedon():
-	pass
+	if !onGotPlacedOn:
+		return
+	callFunc()
 	
 func drawn():
-	pass
+	if !onDrawn:
+		return
+	callFunc()
 
 func bought():
-	pass
+	if !onBought:
+		return
+	callFunc()
 
+
+const FLAGS: Array[String] = ['MakeDamage','MoreCards','MoreEnergie','mogg']  # <-- nur hier ändern
+
+var Actions: Array[String] = []
+var _Actions_bits: int = 0
+
+func _get_property_list() -> Array[Dictionary]:
+	return [
+		{
+			"name": "Actions Picker",
+			"type": TYPE_NIL,
+			"usage": PROPERTY_USAGE_GROUP,
+			"hint_string": ""  # pflichtfeld, auch wenn leer
+		},
+		{
+			"name": "_Actions_bits",
+			"type": TYPE_INT,
+			"usage": PROPERTY_USAGE_EDITOR | PROPERTY_USAGE_STORAGE | PROPERTY_USAGE_SCRIPT_VARIABLE,
+			"hint": PROPERTY_HINT_FLAGS,
+			"hint_string": ",".join(FLAGS)
+		}
+	]
+
+func _get(property: StringName):
+	if property == "_Actions_bits":
+		return _Actions_bits
+
+func _set(property: StringName, value) -> bool:
+	if property == "_Actions_bits":
+		_Actions_bits = value
+		Actions = []
+		for i in FLAGS.size():
+			if value & (1 << i):
+				Actions.append(FLAGS[i])
+		return true
+	return false
+	
 ### cook
 func mogg():
 	print("mogged")
@@ -43,16 +98,8 @@ func MoreCards():
 	
 func MoreEnergie():
 	print('Energy ++')
-	
-enum actions {
-	MakeDamage,
-	MoreCards,
-	MoreEnergie,
-	mogg
-}
-
-@export var Action: actions
 
 
-func callFunc():	
-	call(actions.keys()[Action])
+func callFunc():
+	for action in Actions:
+		call(action)
