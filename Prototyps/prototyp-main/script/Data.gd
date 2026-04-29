99783608570bd70ae8de75c977bcd9fdfc14f689
COOKIE <dominiknageler343@gmail.com>
2026-04-29 16:48:57 +0200

    wip

--- Prototyps/prototyp-main/script/Data.gd
+++ Prototyps/prototyp-main/script/Data.gd
@@ -6,6 +6,32 @@ var Handcards = []
 
 var pile: Array[Variant]
 
+#abfeuer
+var Money			:= 0
+
+var Energy			:= 0
+var EnergyMod		:= 0
+
+var Handsize        := 8
+var HandsizeMod     := 0
+
+var DamageFlat      := 0
+var DamageFlatMod   := 0
+
+var DamageMult      := 0
+var DamageMultMod   := 0
+
+var Firerate        := 0
+var FirerateMod     := 0
+
+var Bulletsize      := 0
+var BulletsizeMod   := 0
+
+var Bulletspeed     := 0
+var BulletspeedMod  := 0
+
+
+
 func get_cards_list():
 	var cards = []
 	for layer in pile:
