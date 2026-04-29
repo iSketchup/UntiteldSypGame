d07988e830adf6559a1f0208767ab24152062829
COOKIE <dominiknageler343@gmail.com>
2026-04-29 16:51:35 +0200

    Data is am abfeuern !!!!!!!!!!!!!!!!

--- Prototyps/prototyp-main/script/States/day.gd
+++ Prototyps/prototyp-main/script/States/day.gd
@@ -1,19 +1,17 @@
 class_name dayState
 extends State
 var Statename := 'day'
-# Unnötig Gerade in Display
-var card_list =get_cards_list()
-var Deck: ItemList
-var HandCards: ItemList
 
 func enter(curPile):
 	Get_HandCards()
 	super.trigger_action("drawn")
 		
 func Get_HandCards():
-	var Cards_count: int
+	var Handcards : ItemList
+	var Cards_count: int = 5 # Das soll in Data
 	for card in range(Cards_count):
 		HandCards.append(Deck[Deck.pick_random])
+	Data.Handcards = Handcards
 	
 func exit():
 	for card in card_list:
